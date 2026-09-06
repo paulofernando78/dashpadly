d8e45cf5461793e362f02bed6d459f949b370822
Paulo Silva <paulofernando78@gmail.com>
2026-09-05 22:52:24 -0300

    wip

--- src/components/features/Widget/WidgetCatalog.jsx
+++ src/components/features/Widget/WidgetCatalog.jsx
@@ -6,7 +6,7 @@ import { WaterIntake } from "@/components/features/WaterIntake";
 import { Calculator } from "@/components/features/Calculator";
 import { Markets } from "@/components/features/Markets";
 
-const bgFrom = 500;
+const bgFrom = 400;
 const bgTo = 600;
 
 const widgetColors = {
